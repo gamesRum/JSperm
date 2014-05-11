bd256622f0fb3d2150c47edffa2c2beafcb3e446
tmartinez <tmartinez@switchfly.com>
2014-05-11 21:20:57 +0000

    codes CRUD added.Squashed commit of the following:
    commit 9ed861e9487d4cb52eb609aa656e1a69e4f06b6f
    Author: tmartinez <tmartinez@switchfly.com>
    Date:   Sun May 11 21:20:04 2014 +0000
    
        Error with js:hint fixed
    
    commit 76acd1f70d07e9d6d613cc0a6abb4d8b2796f2d0
    Author: tmartinez <tmartinez@switchfly.com>
    Date:   Sun May 11 21:16:34 2014 +0000
    
        Mongo handler. Codes crud. Refactor.

--- index.js
+++ index.js
@@ -3,6 +3,33 @@
   'use strict';
 
   var server = require('./lib/jsperm.js');
+    // readline = require('readline'),
+    // rl = readline.createInterface(process.stdin, process.stdout);
 
   server.start();
+
+  // rl.setPrompt('jsperm> ');
+  // rl.prompt();
+  // rl.on('line', function (line) {
+  //   switch (line) {
+  //     case 'help':
+  //       console.log('not yet');
+  //     break;
+  //     case 'restart':
+  //       server.restart();
+  //     break;
+  //     case 'close':
+  //       rl.close();
+  //     break;
+  //     default:
+  //       if (line) {
+  //         console.log('command not found: ' + line);
+  //       }
+  //     break;
+  //   }
+  //   rl.prompt();
+  // }).on('close', function () {
+  //   server.close();
+  //   process.exit(0);
+  // });
 }());
